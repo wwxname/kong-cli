57be3af106fcf20a9e5f0d99f48246afa2f422cf
wwxname <1074424012@qq.com>
2019-05-25 12:40:28 +0800

    kong init

--- src/KongApp.js
+++ src/KongApp.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 import About from './containers/about';
 import Inbox from './containers/inbox';
 import Home from './containers/home';
 
 
-
-
 class KongApp extends React.Component {
+    connect(){
+
+        axios.get(this.urlInput.value)
+    }
     constructor(props) {
         super(props);
         this.state = {route: window.location.hash.substr(1)};
+        this.connect = this.connect.bind(this);
     }
 
     componentDidMount() {
@@ -35,17 +39,27 @@ class KongApp extends React.Component {
                 Child = Home;
         }
 
+
         return (
             <div>
-                <h1>KongApp</h1><input></input>
-                <ul>
-                    <li><a href="#/about">About</a></li>
-                    <li><a href="#/inbox">Inbox</a></li>
-                    <li><a href="#/home">Home</a></li>
-                </ul>
+                <div className={"header"}>
+                    <h1 style={{width:'25px',display:'inline'}}>Kong</h1>
+                    <input ref={input => this.urlInput = input} style={{position: 'relative',left:'100px'}}></input>
+                    <button onClick={this.connect} style={{position: 'relative',left:'100px'}} >链接</button>
+                    <span style={{position: 'relative',left:'150px',color:'red'}}>未连接</span>
+                    <span style={{position: 'relative',left:'200px',color:'red'}}>暂无</span>
+                </div>
+
+                <div className={"topnav"}>
+                    <a href="#/home"> Home </a>
+                    <a href="#/about">About</a>
+                    <a href="#/inbox"> Inbox </a>
+                </div>
                 <Child/>
             </div>
+
         )
     }
 }
+
 export default KongApp;
